dc3b1855b510774718801c41c8eee52fcaa2aa7f
Ercogx <ercogx@gmail.com>
2025-03-14 02:54:42 +0200

    Add Ukrainian translation

---
+++ resources/lang/uk/translations.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'form' => [
+        'actions' => [
+            'save' => 'Зберегти зміни',
+        ],
+    ],
+    'page' => [
+        'title' => 'Налаштування',
+        'navigation_label' => 'Налаштування',
+    ],
+    'notifications' => [
+        'saved' => 'Збережено',
+    ],
+];
