482908f6dc2ee4d2e4587e2e043440341c54fa80
corean <corean@corean.biz>
2025-04-07 13:50:55 +0900

    add Korean language

---
+++ resources/lang/ko/translations.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'form' => [
+        'actions' => [
+            'save' => '변경사항 저장',
+        ],
+    ],
+    'page' => [
+        'title' => '설정',
+        'navigation_label' => '설정',
+    ],
+    'notifications' => [
+        'saved' => '저장되었습니다',
+    ],
+];
