d427f4c8b765b8a2f9b104f2d1525f24b6f188bd
ramersonw <ramersonw@hotmail.com>
2025-04-15 09:19:11 -0300

    add Brazilian Portuguese language

---
+++ resources/lang/pt_BR/translations.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'form' => [
+        'actions' => [
+            'save' => 'Salvar Alterações',
+        ],
+    ],
+    'page' => [
+        'title' => 'Configurações',
+        'navigation_label' => 'Configurações',
+    ],
+    'notifications' => [
+        'saved' => 'Salvo',
+    ],
+];
